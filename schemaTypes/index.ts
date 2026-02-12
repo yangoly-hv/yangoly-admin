e1c615e27f3ece6f8c86ce514444ccc11560b919
Shade <kulakaa073@gmail.com>
2026-02-12 12:23:36 +0200

    feat: removing aboutFounders schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -12,7 +12,6 @@ import collection from './collection';
 import donator from './donator';
 import perfomance from './perfomance';
 import aboutFoundation from './aboutFoundation';
-import aboutFounders from './aboutFounders';
 import donateOrder from './donateOrder';
 
-export const schemaTypes = [donateOrder, aboutFounders, aboutFoundation, perfomance, donator, collection, events, tail, post, report, blockContent, localizedString, localizedBlockContent]
+export const schemaTypes = [donateOrder, aboutFoundation, perfomance, donator, collection, events, tail, post, report, blockContent, localizedString, localizedBlockContent]
